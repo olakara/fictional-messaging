a979d43a43c09d0c7fb4b5a4cce602403abb27a6
Abdel Raoof <olakara@gmail.com>
2022-05-16 22:19:02 +0400

    fix: minor bugs and nonstop sending

--- sender.js
+++ sender.js
@@ -1,32 +1,39 @@
 var amqp = require('amqplib');
 const { v4: uuidv4 } = require('uuid');
-const faker = require('@faker-js/faker');
+const { faker } = require('@faker-js/faker');
 
 
 async function main() {
-    const queue = 'hello';
-    try {
-        let connection = await amqp.connect('amqp://localhost');
-        let channel = await connection.createChannel();
-        await channel.assertQueue(queue, {
-            durable: false
-        });
-        let msg = {
-            type: 'PERSON',
-            event: 'ADD',
-            payload: {
-                id: uuidv4(),
-                name: faker.name.findName(),
-                time: new Date().toLocaleTimeString()
-            }
-        }
-        await channel.sendToQueue(queue, Buffer.from(JSON.stringify(msg)));
-        console.log('[x] Sent %s', msg);
-        await channel.close();
-        await connection.close();
+    
+    try {               
+        myInterval = setInterval(sendMessage,2500);
     } catch (error) {
         console.error(error);
     }
+}
+
+async function sendMessage() {
+
+    let connection = await amqp.connect('amqp://localhost');
+    let channel = await connection.createChannel();
+    
+    await channel.assertQueue('hello', {
+        durable: false
+    });
+
+    let msg = {
+        type: 'PERSON',
+        event: 'ADD',
+        payload: {
+            id: uuidv4(),
+            name: faker.name.findName(),
+            time: new Date().toLocaleTimeString()
+        }
+    }
+    await channel.sendToQueue('hello', Buffer.from(JSON.stringify(msg)));
+    console.log('[x] Sent %s', msg);  
+    await channel.close();
+    await connection.close();
 }
 
 main();
